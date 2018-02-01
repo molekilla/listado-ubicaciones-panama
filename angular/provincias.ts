12ed157415cfc0f7c2512e4be32c92eb81cae2ec
Rogelio Morrell <molekilla@gmail.com>
2018-02-01 12:54:00 -0500

    Add files via upload

---
+++ angular/provincias.ts
@@ -0,0 +1,14 @@
+export const provincias = [{ id: 1, name: 'Bocas del Toro' },
+{ id: 4, name: 'Chiriquí' },
+{ id: 2, name: 'Coclé' },
+{ id: 3, name: 'Colón' },
+{ id: 11, name: 'Comarca Emberá' },
+{ id: 10, name: 'Comarca Kuna Yala' },
+{ id: 12, name: 'Comarca Ngäbe Buglé' },
+{ id: 5, name: 'Darién' },
+{ id: 6, name: 'Herrera' },
+{ id: 7, name: 'Los Santos' },
+{ id: 8, name: 'Panamá' },
+{ id: 13, name: 'Panamá Oeste' },
+{ id: 9, name: 'Veraguas' }
+];
